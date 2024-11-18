c88661e792d635d4c1d3c96c7e91c86cf86ea20e
Harunmansuri <141892054+Harunmansuri@users.noreply.github.com>
2024-11-18 08:24:11 +0530

    currency converter

---
+++ currency-converter/src/App.jsx
@@ -0,0 +1,14 @@
+import Converterform from "./components/Converterform";
+
+function App() {
+  return (
+    <>
+      <div className="currency-converter">
+        <h2 className="converter-title">Currency converter</h2>
+        <Converterform/>
+      </div>
+    </>
+  );
+}
+
+export default App;
